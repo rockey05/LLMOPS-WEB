6468cf788e35f21d2e63c9fc916d38aaceebb75b
rockey <rockeysen000@gmail.com>
2024-07-24 14:57:16 +0530

    added new animation

---
+++ script.js
@@ -0,0 +1,16 @@
+const title = new SplitType("#abttitle");
+const info = new SplitType("#abtinfo")
+
+gsap.to('.abttitle .char', {
+  y: 0,
+  // stagger:0.02,
+  delay: 0,
+  duration: .1
+},)
+
+gsap.to('#row1 .char', {
+  y: 0,
+  // stagger:0.004,
+
+
+},)
