8acfebc9fae8850849c3e7744500c9987d374e75
rockey <rockeysen000@gmail.com>
2024-07-29 14:42:36 +0530

    final changes

--- script.js
+++ script.js
@@ -4,30 +4,38 @@ const info = new SplitType("#abtinfo");
 const triggerAnimations = () => {
   gsap.to('.abttitle .char', {
     y: 0,
-    duration: .1
+    duration: .1,
+    ease: "power2.out"
   });
 
   gsap.to('#row0 .char', {
     y: 0,
-    duration: 1
+    duration: 1,
+    ease: "power2.out"
   });
 
   gsap.to('#row1 .char', {
     y: 0,
-    duration: 1
+    duration: 1,
+    ease: "power2.out"
   });
 
   gsap.to('#row2 .char', {
     y: 0,
     delay: 0.5,
-    duration: 1
+    duration: 1,
+    ease: "power2.out"
   });
 };
 
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      triggerAnimations();
+      // Debouncing the animation trigger
+      if (!entry.target.animationTriggered) {
+        entry.target.animationTriggered = true;
+        triggerAnimations();
+      }
       observer.disconnect(); // Stop observing after animation is triggered
     }
   });
