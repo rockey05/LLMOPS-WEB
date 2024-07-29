7d5010ade80d6e397b89a7059d863e3895032357
rockey <rockeysen000@gmail.com>
2024-07-29 10:33:54 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 const title = new SplitType("#abttitle");
-const info = new SplitType("#abtinfo")
+const info = new SplitType("#abtinfo");
 
-gsap.to('.abttitle .char', {
-  y: 0,
-  // stagger:0.02,
-  delay: 0,
-  duration: .1
-},)
+const triggerAnimations = () => {
+  gsap.to('.abttitle .char', {
+    y: 0,
+    duration: .1
+  });
 
-gsap.to('#row1 .char', {
-  y: 0,
-  // stagger:0.004,
+  gsap.to('#row0 .char', {
+    y: 0,
+    duration: 1
+  });
 
+  gsap.to('#row1 .char', {
+    y: 0,
+    duration: 1
+  });
 
-},)
+  gsap.to('#row2 .char', {
+    y: 0,
+    delay: 0.5,
+    duration: 1
+  });
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      triggerAnimations();
+      observer.disconnect(); // Stop observing after animation is triggered
+    }
+  });
+}, { threshold: 0.1 });
+
+observer.observe(document.querySelector('.content-container'));
